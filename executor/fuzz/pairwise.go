470d2d8d2f713a98a7aa29f4e0122b37bf6312e8
pingzhu <zp_sean@qq.com>
2018-08-24 10:32:51 +0800

    pairewise for fuzz

--- executor/fuzz/pairwise.go
+++ executor/fuzz/pairwise.go
@@ -17,8 +17,9 @@ import (
 )
 
 // Below is for pairwise data
-type PairWises []PairWise
-
+type PairWises struct {
+    PairWises map[string][]PairWise
+}
 
 type PairWise struct {
     PwLength int
@@ -27,31 +28,152 @@ type PairWise struct {
     PwValues []interface{}
 }
 
-// type PairWise struct {
-//     PwLength int
-//     PwElementIndices []int
-//     PwValues []interface{}
+// to define the item for each element
+type PairWiseTestCaseDataSet []PairWiseTestCaseData
+
+type PairWiseTestCaseData []Item
+
+type Item struct {
+    Id string
+    Value interface{}
+    Weights []int
+}
+
+
+// to get the total number of pairwise combinations
+// func GetMaxPairWiseCombinationNumber(combs [][]interface{}, PwLength int) {
+
 // }
 
-func (pws PairWises) ContainsVectorIndex (pos int) (bool, int) {
-    var ifContains bool
-    ifContains = false
-    var pairWiseIndex int
+// to get the item matrix with type Item
+// func GetWorkingItemMatrix(combs [][]interface{}) {
 
-    for i, pairWise := range pws {
-        for _, v_ind := range pairWise.PwVectorIndices {
-            if pos == v_ind {
-                ifContains = true
-                pairWiseIndex = i
-                break
-            }
-        }
-        if ifContains == true {
-            break
-        }
-    }
-    return ifContains, pairWiseIndex
-}
+// }
+
+
+// -------------------------------------------------------------------------
+// The algorithm 1 is rewitten the code for AllPairs (python) using Golang
+// refer to: https://github.com/thombashi/allpairspy
+// -------------------------------------------------------------------------
+
+// func NextPairWiseTestCaseData(combs [][]interface{}, PwLength int):
+//     maxUniquePairsExpected := GetMaxPairWiseCombinationNumber(combs [][]interface{}, PwLength int)
+
+//     // assert len(self.__pairs) <= self.__max_unique_pairs_expected
+
+//     if len(self.__pairs) == self.maxUniquePairsExpected {
+//         return
+//     }
+
+//     workingItemMatrix := GetWorkingItemMatrix(combs)
+
+//     previousUniquePairsCount = len(self.__pairs)
+//     chosenValuesArr := make([]interface{}, len(workingItemMatrix))
+//     indexes := make([]int, len(workingItemMatrix))
+
+//     direction := 1
+//     i := 0
+
+//     for {
+//         if direction == 1:
+//             // move forward
+//             self.__resort_working_array(chosenValuesArr[:i], i)
+//             indexes[i] = 0
+//         elif direction == 0 or direction == -1:
+//             // scan current array or go back
+//             indexes[i] += 1
+//             if indexes[i] >= len(workingItemMatrix[i]):
+//                 direction = -1
+//                 if i == 0:
+//                     raise StopIteration()
+//                 i += direction
+//                 continue
+//             direction = 0
+//         else:
+//             raise ValueError("next(): unknown 'direction' code '{}'".format(direction))
+
+//         chosenValuesArr[i] = workingItemMatrix[i][indexes[i]]
+
+//         if self.__filter_func(self.__get_values_array(chosenValuesArr[:i + 1])):
+//             assert direction > -1
+//             direction = 1
+//         else:
+//             direction = 0
+//         i += direction
+
+//         // to break the for if ...
+//         if i < -1 || i > len(workingItemMatrix) {
+//             break
+//         }
+//     }
+   
+//     if len(workingItemMatrix) != len(chosenValuesArr):
+//         raise StopIteration()
+
+//     self.__pairs.add_sequence(chosenValuesArr)
+
+//     if len(self.__pairs) == previousUniquePairsCount:
+//         // could not find new unique pairs - stop
+//         raise StopIteration()
+
+//     // replace returned array elements with real values and return it
+
+//     return self.__get_iteration_value(chosenValuesArr)
+
+
+// func resortWorkingArray (chosenValuesArr []interface{}, num int) {
+//     for item in workingItemMatrix[num]:
+//         data_node = self.__pairs.get_node_info(item)
+
+//         new_combs = [
+//             // numbers of new combinations to be created if this item is
+//             // appended to array
+//             set([key(z) for z in combinations(chosen_values_arr + [item], i + 1)])
+//             - self.__pairs.get_combs()[i]
+//             for i in range(0, self.__n)
+//         ]
+
+//         // weighting the node
+//         // node that creates most of new pairs is the best
+//         item.weights = [-len(new_combs[-1])]
+
+//         // less used outbound connections most likely to produce more new
+//         // pairs while search continues
+//         item.weights += (
+//             [len(data_node.out)]
+//             + [len(x) for x in reversed(new_combs[:-1])]
+//             + [-data_node.counter]  # less used node is better
+//         )
+
+//         // otherwise we will prefer node with most of free inbound
+//         // connections; somehow it works out better ;)
+//         item.weights.append(-len(data_node.in_))
+
+//     workingItemMatrix[num].sort(key=cmp_to_key(cmp_item))
+// }
+
+
+
+
+// func (pws PairWises) ContainsVectorIndex (pos int) (bool, int) {
+//     var ifContains bool
+//     ifContains = false
+//     var pairWiseIndex int
+
+//     for i, pairWise := range pws {
+//         for _, v_ind := range pairWise.PwVectorIndices {
+//             if pos == v_ind {
+//                 ifContains = true
+//                 pairWiseIndex = i
+//                 break
+//             }
+//         }
+//         if ifContains == true {
+//             break
+//         }
+//     }
+//     return ifContains, pairWiseIndex
+// }
 
 func (pw PairWise) ContainsVectorIndex (pos int) bool {
     var ifContains bool
@@ -101,15 +223,16 @@ func GetPairWiseValid22(fuzzData FuzzData, PwLength int) {
 
 func GetPairWise12 (indexCombs chan []int, combos [][]interface{}, PwLength int) {
     var pairWises PairWises
+    mapp := make(map[string][]PairWise)
 
-    for value := range indexCombs {
+    for indexPair := range indexCombs {
         var pairWise PairWise
         pairWise.PwLength = PwLength
-        pairWise.PwVectorIndices = value
+        pairWise.PwVectorIndices = indexPair
         
         var combos_pw_index_slice [][]interface{}
 
-        for _, ind_value := range value {
+        for _, ind_value := range indexPair {
             var indexSlice []interface{}
             for i, _ := range combos[ind_value] {
                 indexSlice = append(indexSlice, i)
@@ -129,6 +252,7 @@ func GetPairWise12 (indexCombs chan []int, combos [][]interface{}, PwLength int)
         fmt.Println("c: ", c, len(c))
 
         i := 0
+        
         for pairwise := range c {
             i = i + 1
             // fmt.Println("results_pairwise: ", pairwise, len(pairwise), pairwise[0], pairwise[1])
@@ -137,22 +261,35 @@ func GetPairWise12 (indexCombs chan []int, combos [][]interface{}, PwLength int)
             for ii, ind_value := range pairwise {
                 pwind = append(pwind, ind_value.(int))
 
-                pairwiseValue = append(pairwiseValue, combos[value[ii]][ind_value.(int)])
+                pairwiseValue = append(pairwiseValue, combos[indexPair[ii]][ind_value.(int)])
             }
             pairWise.PwElementIndices = pwind
             pairWise.PwValues = pairwiseValue
             fmt.Println("pairwiseValue length: ", pairwiseValue, i)
 
-            pairWises = append(pairWises, pairWise)
+
+            ///
+            str := ""
+            for _, ind_value := range indexPair {
+                str = str + fmt.Sprint(ind_value)
+            }
+            // fmt.Println("pairWises length: ", fmt.Sprint(indexPair))
+            mapp[str] = append(mapp[str], pairWise)
+            // pairWises.PairWises[str] = append(pairWises.PairWises[str], pairWise)
+            pairWises.PairWises = mapp
         }
     }
-    fmt.Println("pairWises length: ", pairWises, len(pairWises))
+    fmt.Println("pairWises length: ", pairWises)
     // return pairWises
 }
 
-func GeneratePairWiseTestData () {
+// func GeneratePairWiseTestData () {
+//     // now we have the informations about:
+//     // 1. pairwise length (N), the test case length (M)
+//     // 2. the pairWises, which is grouped by sub-combinations
+//     // 3. the total number of the sub-combinations
 
-}
+// }
 
 
 
@@ -164,6 +301,7 @@ func GeneratePairWiseTestData () {
 // --> algorithm 2. get the full combination first, the filter (remove) them based on the pairWises if repeated
 // -------------------------------------------------------------
 // Here uses the algorithm 2
+// Warning: after testing, this algorithm has performance issue, whil also can not result minimal set of pairwise test data (case)
 
 func GetPairWiseValid(fuzzData FuzzData, PwLength int) {
     var combos [][]interface{}
