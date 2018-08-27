ddeb7d524be47e119261a2263abfcb2987b6d0ab
pingzhu <zp_sean@qq.com>
2018-08-27 16:47:15 +0800

    mutation

--- executor/fuzz/mutation.go
+++ executor/fuzz/mutation.go
@@ -17,92 +17,61 @@ import (
     // "path/filepath"
     // "strings"
     // "strconv"
+    "encoding/json"
     "go4api/testcase"  
 )
 
 // mutation is to mutate the valid data to working api, see if mutated invalid data still can be handled by the api
-type MutationField struct {
-    MuChar string
-    MuInt int64
-}
+// func (tcData testcase.TestCaseDataInfo) MutateRequestMethod () {
+//     tcData.TestCase.SetRequestMethod("DELETE")
 
+// }
 
-type MutationTestCase struct {
-    TestCase testcase.TestCase
-}
+// func (muTc testcase.TestCaseDataInfo) MutateRequestPath () {
+//     tcData.TestCase.SetRequestPath("/aa/bb/cc")
+// }
 
-func (muTc MutationTestCase) MutateRequestMethod () {
-    muTc.TestCase.SetRequestMethod("DELETE")
+// two ways to mutate the testcase:
+// Option 1: 
+// copy the underlying fields and values to another TestCaseDataInfo, with mutation(s)
+// the better way would be deep copy the TestCaseDataInfo, and change the value, but Golang standard
+// Lib has no deepcopy, so that, here use a plain way, that is, to re-sturct the TestCaseDataInfo
+//
+// Option 2:
+// json.Marshal the tc in originMutationTcArray, 
+// then change the value(s) in the json
+// then Unmarshal the to testcase, and add to mutatedTcArray
+// then execute the mutatedTcArray
 
-    // "request": {
-    //     "method": "GET",
-    //     "path": "/api/operation/delivery-terms",
-    //     "headers": {
-    //       "authorization": "{{.authorization}}"
-    //     },
-    //     "queryString": {
-    //       "pageIndex": "1",
-    //       "pageSize": "12"
-    //     }
-}
-
-func (muTc MutationTestCase) MutateRequestPath () {
-    muTc.TestCase.SetRequestPath("/aa/bb/cc")
-}
-
+// focus on the Request to mutate
+// type Request struct {  
+//     Method string
+//     Path string
+//     Headers map[string]interface{}
+//     QueryString map[string]interface{}
+//     Payload map[string]interface{}
+// }
 
 func MutateTcArray(originMutationTcArray []testcase.TestCaseDataInfo) []testcase.TestCaseDataInfo {
     var mutatedTcArray []testcase.TestCaseDataInfo
 
     for _, originTcData := range originMutationTcArray {
-        // here copy the underlying fields and values to another TestCaseDataInfo, with mutation(s)
-        // the better way would be deep copy the TestCaseDataInfo, and change the value, but Golang standard
-        // Lib has no deepcopy, so that, here use a plain way, that is, to re-sturct the TestCaseDataInfo
+        tcJson, _ := json.Marshal(originTcData)
+        fmt.Println(string(tcJson)) 
 
-
-        // type Request struct {  
-        //     Method string
-        //     Path string
-        //     Headers map[string]interface{}
-        //     QueryString map[string]interface{}
-        //     Payload map[string]interface{}
-        // }
-        // var mtcRequest testcase.Request
-        // mtcRequest.Method = originTcData.TestCase.ReqMethod()
-        // mtcRequest.Path = originTcData.TestCase.ReqPath()
-
-        // mtcRequest.Headers = originTcData.TestCase.DelRequestHeader("authorization")
-
-        // mtcRequest.QueryString = originTcData.TestCase[originTcData.TcName()].Request.QueryString
-        // mtcRequest.Payload = originTcData.TestCase.ReqPayload()
-
-
-        var mTcBasics testcase.TestCaseBasics
-        
-        mTcBasics.Priority = originTcData.TestCase[originTcData.TcName()].Priority
-        mTcBasics.ParentTestCase = originTcData.TestCase[originTcData.TcName()].ParentTestCase
-        mTcBasics.Inputs = originTcData.TestCase[originTcData.TcName()].Inputs
-        // Request
-        mTcBasics.Request = originTcData.TestCase[originTcData.TcName()].Request
-        // Response
-        mTcBasics.Response = originTcData.TestCase[originTcData.TcName()].Response
-        mTcBasics.Outputs = originTcData.TestCase[originTcData.TcName()].Outputs
-
-
-
-        originTcData.TestCase.DelRequestHeader("authorization")
-
-        mTc := testcase.TestCase{}
-        mTc[originTcData.TcName()] = mTcBasics
 
         var mTcData testcase.TestCaseDataInfo
-        mTcData.TestCase = mTc
-        mTcData.JsonFilePath = originTcData.JsonFilePath
-        mTcData.CsvFile = originTcData.CsvFile
-        mTcData.CsvRow = originTcData.CsvRow
 
-        fmt.Println("TcData: ", mTcData, originTcData)
+        json.Unmarshal(tcJson, &mTcData)
+        fmt.Println(mTcData)
+
+        mTcData.TestCase.AddRequestHeader("aaaa", "dbddsdsfa")
+
+        fmt.Println(mTcData)
+
         mutatedTcArray = append(mutatedTcArray, mTcData)
+
+        break
     }
 
     return mutatedTcArray
