f203043a17e328aa81a5b53d561b5d2e93ba0540
pingzhu <zp_sean@qq.com>
2018-09-06 11:44:30 +0800

    refactor the http using type interface

--- protocal/http/http.go
+++ protocal/http/http.go
@@ -20,24 +20,36 @@ import (
     "bytes"
 )
 
+type HttpRequest interface {
+    Request(urlStr string, apiMethod string, reqHeaders map[string]interface{}, reqBody interface{}) (int, http.Header, []byte)
+}
 
-func HttpGet(urlStr string, apiMethod string, reqHeaders map[string]interface{}, reqBody *strings.Reader) (int, http.Header, []byte) { 
+type HttpRestful struct{}
+
+func (httpRestful HttpRestful) Request(urlStr string, apiMethod string, reqHeaders map[string]interface{}, reqBody interface{}) (int, http.Header, []byte) { 
     //client 
     client := &http.Client{}
-
-    // payload := reqBody
-    // fmt.Println("urlStr", urlStr)
     //
-    reqest, err := http.NewRequest(apiMethod, urlStr, nil)
-
+    // type conversion to payload, based on reqBody, apiMethod
+    var reqest *http.Request
+    var err error
+    switch reflect.TypeOf(reqBody).String() {
+        case "*strings.Reader":
+            if apiMethod == "GET" {
+                reqest, err = http.NewRequest(apiMethod, urlStr, nil)
+            } else {
+                reqest, err = http.NewRequest(apiMethod, urlStr, reqBody.(*strings.Reader))
+            }
+        case "*bytes.Buffer":
+            reqest, err = http.NewRequest(apiMethod, urlStr, reqBody.(*bytes.Buffer))
+    }
+    if err != nil {
+        panic(err)
+    }
     //Header
     for key, value := range reqHeaders {
         reqest.Header.Add(key, fmt.Sprint(value))
     }
-    // fmt.Println("url", url)
-    // fmt.Println("payload", payload)
-    // fmt.Println("reqest.Header", reqest.Header)
-
     //response
     response, err := client.Do(reqest)
     if err != nil {
@@ -45,152 +57,8 @@ func HttpGet(urlStr string, apiMethod string, reqHeaders map[string]interface{},
     } 
     defer response.Body.Close()
 
-    //func ReadAll(r io.Reader) ([]byte, error)
     body, _ := ioutil.ReadAll(response.Body)
 
     return response.StatusCode, response.Header, body
 }
-
-func HttpPost(urlStr string, apiMethod string, reqHeaders map[string]interface{}, reqBody *strings.Reader) (int, http.Header, []byte) { 
-    //client 
-    client := &http.Client{}
-    //
-    payload := reqBody
-    // fmt.Println("urlStr", urlStr)
-    // fmt.Println("payload", payload)
-
-    reqest, err := http.NewRequest(apiMethod, urlStr, payload)
-
-    //Header
-    for key, value := range reqHeaders {
-        reqest.Header.Add(key, fmt.Sprint(value))
-    }
-    
-    // fmt.Println("reqest.Header", reqest.Header)
-
-    //response
-    response, err := client.Do(reqest)
-    if err != nil {
-        panic(err)
-    }
-    defer response.Body.Close()
-    body, _ := ioutil.ReadAll(response.Body)
-    // fmt.Println(response.Header)
-    // fmt.Println(response.StatusCode)
-    // fmt.Println(string(body))
-
-    return response.StatusCode, response.Header, body
-}
-
-func HttpPostForm(urlStr string, apiMethod string, reqHeaders map[string]interface{}, reqBody *strings.Reader) (int, http.Header, []byte) { 
-    //client 
-    client := &http.Client{}
-    //
-    payload := reqBody
-
-    reqest, err := http.NewRequest(apiMethod, urlStr, payload)
-
-    //Header
-    for key, value := range reqHeaders {
-        reqest.Header.Add(key, fmt.Sprint(value))
-    }
-    // fmt.Println("url", url)
-    // fmt.Println("payload", payload)
-    // fmt.Println("reqest.Header", reqest.Header)
-
-    //response
-    response, err := client.Do(reqest)
-    if err != nil {
-        panic(err)
-    }  
-    defer response.Body.Close()
-    body, _ := ioutil.ReadAll(response.Body)
-    // fmt.Println(response.Header)
-    // fmt.Println(response.StatusCode)
-    // fmt.Println(string(body))
-
-    return response.StatusCode, response.Header, body
-}
-
-func HttpPostMultipart(urlStr string, apiMethod string, reqHeaders map[string]interface{}, reqBody *bytes.Buffer) (int, http.Header, []byte) { 
-    //client 
-    client := &http.Client{}
-    //
-    payload := reqBody
-    
-    reqest, err := http.NewRequest(apiMethod, urlStr, payload)
-
-    //Header
-    for key, value := range reqHeaders {
-        reqest.Header.Add(key, fmt.Sprint(value))
-    }
-    // fmt.Println("url", url)
-    // fmt.Println("payload", payload)
-    // fmt.Println("reqest.Header", reqest.Header)
-
-    //response
-    response, err := client.Do(reqest)
-    if err != nil {
-        panic(err)
-    }  
-    defer response.Body.Close()
-    body, _ := ioutil.ReadAll(response.Body)
-    // fmt.Println(response.Header)
-    // fmt.Println(response.StatusCode)
-    // fmt.Println(string(body))
-
-    // protocalChan <- "aaa"
-
-    return response.StatusCode, response.Header, body
-}
-
-
-func HttpPut(urlStr string, apiMethod string, reqHeaders map[string]interface{}, reqBody *strings.Reader) (int, http.Header, []byte) { 
-    //client 
-    client := &http.Client{}
-    //
-    payload := reqBody
-
-    reqest, err := http.NewRequest(apiMethod, urlStr, payload)
-    // fmt.Println("urlStr", urlStr)
-    //Header
-    for key, value := range reqHeaders {
-        reqest.Header.Add(key, fmt.Sprint(value))
-    }
-    // fmt.Println("urlStr", urlStr)
-    // fmt.Println("payload", payload)
-    // fmt.Println("reqest.Header", reqest.Header)
-
-    //response
-    response, err := client.Do(reqest)
-    if err != nil {
-        panic(err)
-    }  
-    defer response.Body.Close()
-    body, _ := ioutil.ReadAll(response.Body)
-    // fmt.Println(response.Header)
-    // fmt.Println(response.StatusCode)
-    // fmt.Println(string(body))
-
-    return response.StatusCode, response.Header, body
-}
-
-
-func CallHttpMethod(m map[string]interface{}, name string, params ... interface{}) (int, http.Header, []byte) {
-    f := reflect.ValueOf(m[name])
-    // if len(params) != f.Type().NumIn() {
-    //     err = errors.New("The number of params is not adapted.")
-    //     return
-    // }
-    in := make([]reflect.Value, len(params))
-    for k, param := range params {
-        in[k] = reflect.ValueOf(param)
-    }
-
-    result := f.Call(in)
-
-    return result[0].Interface().(int), result[1].Interface().(http.Header), result[2].Interface().([]byte)
-}
-
-
 
